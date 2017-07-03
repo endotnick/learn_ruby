f62d994c73e567863e4c5b71b49a7679bbf1c845
Nick Rodriguez <endotnick@gmail.com>
2017-07-03 22:53:05 +0000

    add solution for problem 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def subtract(num1, num2)
+    num1 - num2
+end
+
+def sum(nums)
+    sum = 0
+    nums.each do |num|
+        sum += num
+    end
+    sum
+end
+
+def multiply(*nums)
+    nums.reduce(1, :*)
+end
+
+def power(num1, num2)
+    num1 ** num2
+end
+
+def factorial(num)
+    (1..(num.zero? ? 1 : num)).inject(:*)
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,48 @@
 #write your code here
+def echo(string)
+    string
+end
+
+def shout(string)
+   string.upcase
+end
+
+def repeat(string, count = 2)
+    phrase = string
+    (count - 1).times do
+        phrase += " #{string}"
+    end
+    phrase
+end
+
+def start_of_word(string, count)
+   i = 0
+   word_start = ""
+   while i < count do
+       word_start += string[i]
+       i += 1
+   end
+   word_start
+end
+
+def first_word(string)
+    string.split(" ")[0] 
+end
+
+def titleize(string)
+    title = ""
+    words = string.split(" ")
+    spaces = words.length - 1
+    little_words = ["and", "of", "the", "to", "by", "a", "over"]
+    
+    words.each_with_index do |word, index| 
+        if (little_words.include? word) && (index != 0)
+            title += word
+        else
+            title += word.capitalize 
+        end
+        title += " " unless (spaces == 0)
+        spaces -= 1
+    end
+    title
+end
