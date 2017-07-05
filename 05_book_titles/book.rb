46d10a1d673e5b6fe35fd9545de0024584e8808a
Nick Rodriguez <endotnick@gmail.com>
2017-07-05 18:34:57 +0000

    add solution for problem 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
 # write your code here
+attr_accessor :title
+
+LittleWords = ['and', 'by', 'the', 'or', 'of', 'in', 'a', 'an']
+
+def title()
+    titleWords = @title.split(" ")
+    spaces = titleWords.length - 1
+    output = ""
+    
+    titleWords.each_with_index do |word, index|
+        if (LittleWords.include? word) && (index != 0)
+            output += word
+        else 
+            output += word.capitalize   
+        end
+        
+        if spaces > 0
+            output += " "
+            spaces -= 1
+        end
+    end
+    output
+end
+
+
+
 end
